14ba3f2eaaf6ded3d7eede53a8e2b9262ccb2e3e
user <user@WIN-RV3D993QKOS>
2025-09-12 19:54:19 +0300

    distance DONE

--- Introduction to OOP/main.cpp
+++ Introduction to OOP/main.cpp
@@ -22,7 +22,22 @@ public:
 	{
 		this ->y = y;
 	}
+	double distance(Point other)
+	{
+		double x_distance = this->x - other.x;
+		double y_distance = this->y - other.y;
+		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+		return distance;
+	}
 };
+
+double distance(Point A, Point B)
+{
+	double x_distance = A.get_x() - B.get_x();
+	double y_distance = A.get_y() - B.get_y();
+	double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+	return distance;
+}
 
 //#define STRUCT_POINT
 
@@ -42,4 +57,14 @@ void main()
 	A.set_x(2);
 	A.set_y(3);
 	cout << A.get_x() << "\t" << A.get_y() << endl;
+
+	Point B;
+	B.set_x(7);
+	B.set_y(8);
+	cout << B.get_x() << "\t" << B.get_y() << endl;
+
+	cout << "Расстояние от точки А до точки В: " << A.distance(B) << endl;
+	cout << "Расстояние от точки B до точки F: " << B.distance(A) << endl;
+	cout << "Расстояние между точками А и В: " << distance(A, B) << endl;
+	cout << "Расстояние между точками B и A: " << distance(B, A) << endl;
 }
